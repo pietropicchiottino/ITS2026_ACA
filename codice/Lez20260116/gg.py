3c0965bb8e21be153ceb5f463ecbcb169c3b23fb
pietropicchiottino <pietro.picchiottino@edu-its.it>
2026-01-29 10:47:36 +0100

    git remove app.py

---
+++ codice/Lez20260116/gg.py
@@ -0,0 +1,23 @@
+# nome = input("Inserisci il tuo nome: ")
+
+# print(f"Il tuo nome è {nome}")
+# print(f"Il tuo nome è {len(nome)} caratteri")
+# print(f"il tuo nome maiuscolo {nome.upper()}")
+# print(f"il tuo nome minuscolo {nome.lower()}")
+# print(f"il tuo nome capitalizzato {nome.capitalize()}")
+
+# for carattere in nome:
+#     print(carattere)
+
+# carattere = input("Inserisci il carattere da usare: ")
+# print(((carattere + ' ') * 4 + '\n') * 4)
+
+# for i in range(4):
+#     for j in range(4):
+#         print(carattere +' ', end = '')
+#     print ()
+
+for i in range(10):
+    print (i)
+    print(range)
+
