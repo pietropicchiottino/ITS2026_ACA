db7e1df790183a49bdcd31b9249a5f7e6fa21458
pietropicchiottino <pietro.picchiottino@edu-its.it>
2026-01-30 10:04:45 +0100

    https://github.com/maboglia/ITS2026_ACA.git

--- codice/Lez20260129/studenti.py
+++ codice/Lez20260129/studenti.py
@@ -1,13 +1,27 @@
-studenti = [    ]
+# studenti = [    ]
 
-f = open('studenti.txt')
+# # Nota l'uso di r'' per evitare problemi con le barre rovesciate di Windows
+# f = open(r'c:\Users\pietro.picchiottino\py-scuola\ITS2026_ACA\codice\Lez20260129\singolostudente.txt')
+
+# for riga in f:
+#     # print(riga)
+#     # per ogni riga del file aggiungo alla lista studenti
+#     studenti.append(studenti)
+
+# f.close()
+
+# for studente in studenti:
+#     print(studente)
+studenti = []
+
+f = open('./singolostudente.txt')
 
 for riga in f:
     # print(riga)
-    # per ogni riga del file aggiungo alla lista studenti
-    studenti.append(studenti)
+    # per ogni riga del file aggiungo la riga alla lista studenti
+    studenti.append(riga)
 
 f.close()
 
-for studente in studenti:
-    print(studenti)
+for s in studenti:
+    print(s)
